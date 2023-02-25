065172836c5b26cfa01d56457ed7b8dde0db7397
Motiejus Jakštys <motiejus@jakstys.lt>
2023-02-25 15:55:08 +0200

    add sr.ht pubkey

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,10 @@ in {
     "github.com" = {
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
+
+    "git.sr.ht" = {
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+    };
   };
 
   services = {
