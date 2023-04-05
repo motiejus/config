c4acd525c7b79424d474816cecd554e469db22f2
Motiejus Jakštys <motiejus@jakstys.lt>
2023-04-05 14:46:57 +0300

    flakes: add age and agenix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,30 @@
 {
   description = "motiejus/config";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11-small";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.inputs.darwin.follows = "";
+
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+    deploy-rs.inputs.utils.follows = "flake-utils";
+
+  };
+
   nixConfig = {
     trusted-substituters = "https://cache.nixos.org/";
     trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
     extra-experimental-features = "nix-command flakes";
   };
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11-small";
-
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
-    deploy-rs.inputs.utils.follows = "flake-utils";
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-  };
-
   outputs = {
     self,
     nixpkgs,
+    agenix,
     deploy-rs,
     flake-utils,
   }: let
@@ -33,6 +37,8 @@
           ./configuration.nix
           ./hardware-configuration.nix
           ./zfs.nix
+
+          agenix.nixosModules.default
         ];
       };
 
@@ -55,7 +61,11 @@
     in {
       devShells.default = with pkgs;
         mkShell {
-          packages = [deploy-rs.packages.x86_64-linux.deploy-rs];
+          packages = [
+              pkgs.age
+              agenix.packages.${system}.agenix
+              deploy-rs.packages.${system}.deploy-rs
+          ];
         };
 
       formatter = pkgs.alejandra;
