7ac126445450c4c5dd4cb7aebb66a571b6a47546
Motiejus Jakštys <motiejus@jakstys.lt>
2023-07-20 16:32:46 +0300

    add motiejus_yk2

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,8 @@
 let
   motiejus_yk1 = "age1yubikey1qtwmhf7h7ljs3dyx06wyzme4st6w4calkdpmsxgpxc9t2cldezvasd6n8wg";
+  motiejus_yk2 = "age1yubikey1qgyvs2ul0enzqf4sscq96zyxk73jnj4lknpemak2hp39lejdwc0s5uzzhpc";
   motiejus_bk1 = "age1kyehn8yr9tfu3w0z4d9p9qrj0tjjh92ljxmz2nyr6xnm7y8kpv5spwwc9n";
-  motiejus = [motiejus_yk1 motiejus_bk1];
+  motiejus = [motiejus_yk1 motiejus_yk2 motiejus_bk1];
 
   hel1-a = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6Wd2lKrpP2Gqul10obMo2dc1xKaaLv0I4FAnfIaFKu";
   systems = [hel1-a];
