7307ef57f78e8b0a5b9a82fb07392807c46ce80d
Motiejus Jakštys <motiejus@jakstys.lt>
2023-07-23 15:37:59 +0300

    comment zfs passphrases temporarily

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
           {
             age.secrets.motiejus-passwd-hash.file = ./secrets/motiejus_passwd_hash.age;
             age.secrets.root-passwd-hash.file = ./secrets/root_passwd_hash.age;
-            age.secrets.zfs-passphrase.vno1-oh2.file = ./secrets/vno1-oh2/zfs-passphrase.age;
+            #age.secrets.zfs-passphrase.vno1-oh2.file = ./secrets/vno1-oh2/zfs-passphrase.age;
 
             age.secrets.borgbackup-password.file = ./secrets/hel1-a/borgbackup/password.age;
             age.secrets.sasl-passwd.file = ./secrets/hel1-a/postfix/sasl_passwd.age;
@@ -86,7 +86,7 @@
           {
             age.secrets.motiejus-passwd-hash.file = ./secrets/motiejus_passwd_hash.age;
             age.secrets.root-passwd-hash.file = ./secrets/root_passwd_hash.age;
-            age.secrets.zfs-passphrase.hel1-a.file = ./secrets/hel1-a/zfs-passphrase.age;
+            #age.secrets.zfs-passphrase.hel1-a.file = ./secrets/hel1-a/zfs-passphrase.age;
           }
         ];
 
