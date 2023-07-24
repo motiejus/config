f5d3249fa0ec071361694243d0f923e53623fb55
Motiejus Jakštys <motiejus@jakstys.lt>
2023-07-24 12:47:41 +0300

    add en_US.UTF-8/UTF-8 locale

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -32,10 +32,10 @@
   config = {
     time.timeZone = config.mj.timeZone;
 
-    # Select internationalisation properties.
     i18n = {
       defaultLocale = "en_US.UTF-8";
       supportedLocales = [
+        "en_US.UTF-8/UTF-8"
         "lt_LT.UTF-8/UTF-8"
       ];
     };
