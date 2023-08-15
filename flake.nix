a11871551f5cda9681add531e40415d606294c35
Motiejus Jakštys <motiejus@jakstys.lt>
2023-08-15 23:12:02 +0300

    roll with non-small for now

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "motiejus/config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     # TODO: called with unexpected argument 'home-manager'
