0bf61ad07c3267da34ce9f2b4f58614d992e757c
Motiejus Jakštys <motiejus@jakstys.lt>
2023-08-17 15:23:49 +0300

    disable cage
    want stdout/stderr

--- hosts/vno1-rp3b/configuration.nix
+++ hosts/vno1-rp3b/configuration.nix
@@ -82,9 +82,9 @@
   };
 
   users.extraUsers.kodi.isNormalUser = true;
-  services.cage.user = "kodi";
-  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
-  services.cage.enable = true;
+  #services.cage.user = "kodi";
+  #services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  #services.cage.enable = true;
   nixpkgs.config.kodi.enableAdvancedLauncher = true;
 
   environment.systemPackages = with pkgs; [
