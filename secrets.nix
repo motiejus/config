85e6fbfadb312ddae53b3f433aa979735527d4e4
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-11 12:01:03 +0300

    vno1-rp3b datadisk passphrase

--- secrets.nix
+++ secrets.nix
@@ -36,6 +36,9 @@ in
   // mk ([fra1-a] ++ motiejus) [
     "secrets/vno1-oh2/zfs-passphrase.age"
   ]
+  // mk ([vno1-rp3b] ++ motiejus) [
+    "secrets/vno1-rp3b/datadisk-passphrase.age"
+  ]
   // mk (systems ++ motiejus) [
     "secrets/motiejus_passwd_hash.age"
     "secrets/root_passwd_hash.age"
