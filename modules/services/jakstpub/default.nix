e12e1391288cdca7cb61cd5f8e0b51d066e548af
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-13 09:00:28 +0300

    samba: make file/dir masks a bit more restrictive

--- modules/services/jakstpub/default.nix
+++ modules/services/jakstpub/default.nix
@@ -34,8 +34,8 @@
             public = "yes";
             "guest ok" = "yes";
             "read only" = "no";
-            "create mask" = "0666";
-            "directory mask" = "0777";
+            "create mask" = "0664";
+            "directory mask" = "0775";
             "force user" = "jakstpub";
             "force group" = "jakstpub";
           };
