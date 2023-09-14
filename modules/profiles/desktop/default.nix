c80b1a996aa2782b6ca214e8c547e2f0c706d564
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-14 21:58:06 +0300

    switch to lightdm/xfce4 + sound

--- modules/profiles/desktop/default.nix
+++ modules/profiles/desktop/default.nix
@@ -17,11 +17,24 @@
     services = {
       xserver = {
         enable = true;
-        desktopManager.gnome.enable = true;
-        displayManager.gdm.enable = true;
+        desktopManager.xfce.enable = true;
+        displayManager.lightdm.enable = true;
+      };
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
       };
     };
 
+    security.rtkit.enable = true;
+
     networking.networkmanager.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      pavucontrol
+    ];
   };
 }
