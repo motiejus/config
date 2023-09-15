831bce0813330686e0c3d3d506411df31ae23c63
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-15 12:32:47 +0300

    move to non-small
    I'd rather not compile firefox.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "motiejus/config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager.url = "github:nix-community/home-manager/release-23.05";
