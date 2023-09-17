7f4fe7b001e04758ba2eb098c797a36d15be0be8
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-17 22:46:38 +0300

    firefox too

--- modules/profiles/desktop/rc.lua
+++ modules/profiles/desktop/rc.lua
@@ -502,13 +502,12 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = false }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
-     { rule = { class = "Signal" },
-       properties = { screen = 1, tag = "9" } },
-     { rule = { class = "Element" },
-       properties = { screen = 1, tag = "9" } },
+    { rule = { class = "Firefox" },
+      properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Signal" },
+      properties = { screen = 1, tag = "9" } },
+    { rule = { class = "Element" },
+      properties = { screen = 1, tag = "9" } },
 }
 -- }}}
 
