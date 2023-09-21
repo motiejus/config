663034652035fd43c955b6c4f5c6a94cab5c1afe
Motiejus Jakštys <motiejus@jakstys.lt>
2023-09-21 06:36:15 +0300

    fwminex: remove docker mountpoint

--- hosts/fwminex/configuration.nix
+++ hosts/fwminex/configuration.nix
@@ -29,11 +29,6 @@ in {
 
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
-  fileSystems."/var/lib/docker" = {
-    device = "rpool/nixos/var/lib/docker";
-    fsType = "zfs";
-  };
-
   #swapDevices = [];
 
   boot.loader.grub.extraEntries = ''
