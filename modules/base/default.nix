a4a68a3d22415564478c7558dad0777f3a439abe
Motiejus Jakštys <motiejus@jakstys.lt>
2023-12-19 23:58:08 +0200

    the correct default locale

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -45,7 +45,7 @@
     ];
 
     i18n = {
-      defaultLocale = "en_US.UTF-8";
+      defaultLocale = "en_US.UTF-8/UTF-8";
       supportedLocales = [
         "C.UTF-8/UTF-8"
         "en_US.UTF-8/UTF-8"
