22ae0836eae803843b2ae580e57e976c6de0152f
Motiejus Jakštys <motiejus@jakstys.lt>
2023-12-20 17:07:54 +0200

    locales: bring back the old means
    otherwise perl is complaining

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -45,9 +45,8 @@
     ];
 
     i18n = {
-      defaultLocale = "en_US.UTF-8/UTF-8";
+      defaultLocale = "en_US.UTF-8";
       supportedLocales = [
-        "C.UTF-8/UTF-8"
         "en_US.UTF-8/UTF-8"
         "lt_LT.UTF-8/UTF-8"
       ];
