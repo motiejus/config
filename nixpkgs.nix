670e11eb7be2b4fd354b3162fe1b923caa32696e
Motiejus Jakštys <motiejus@jakstys.lt>
2024-02-05 13:39:51 +0200

    rm nixpkgs.nix

--- nixpkgs.nix
+++
@@ -1,6 +0,0 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  spec = lock.nodes.nixpkgs.locked;
-  nixpkgs = fetchTarball "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-in
-  import nixpkgs
