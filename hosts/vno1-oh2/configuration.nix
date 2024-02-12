2bc47bbb37bf82ce992523c375425e55e9ba7bec
Motiejus Jakštys <motiejus@jakstys.lt>
2024-02-12 16:09:25 +0200

    soju origin

--- hosts/vno1-oh2/configuration.nix
+++ hosts/vno1-oh2/configuration.nix
@@ -528,7 +528,7 @@
       tlsCertificate = "/run/soju/cert.pem";
       tlsCertificateKey = "/run/soju/key.pem";
       hostName = "irc.jakstys.lt";
-      httpOrigins = ["https://irc.jakstys.lt"];
+      httpOrigins = ["irc.jakstys.lt:${toString mydata.ports.soju-ws}"];
       extraConfig = ''
         message-store fs /var/lib/soju
       '';
