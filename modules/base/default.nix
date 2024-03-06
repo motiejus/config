88da46eb98e8b739e9626e85440a2e89d8f53e67
Motiejus Jakštys <motiejus@jakstys.lt>
2024-03-06 12:56:27 +0200

    base: add bcachefs-tools

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -129,6 +129,7 @@ in {
         lm_sensors
         smartmontools
         unixtools.xxd
+        bcachefs-tools
 
         # networking
         wol
@@ -179,30 +180,6 @@ in {
         vimAlias = true;
         defaultEditor = true;
       };
-
-      # TODO
-      # error: builder for '/nix/store/3d6dl3p6vh6q167f476g0jd5k9lf40vx-sysdig-0.33.1.drv' failed with exit code 2;
-      #        last 10 log lines:
-      #        > make[4]: *** [/nix/store/dx2530rhfk0wpwwvqjxb5bsxjqwrlmv2-linux-6.6.2-dev/lib/modules/6.6.2/source/Makefile:234: __sub-make] Error 2
-      #        > make[3]: *** [Makefile:16: all] Error 2
-      #        > make[2]: *** [driver/CMakeFiles/driver.dir/build.make:70: driver/CMakeFiles/driver] Error 2
-      #        > make[1]: *** [CMakeFiles/Makefile2:602: driver/CMakeFiles/driver.dir/all] Error 2
-      #        > make[1]: *** Waiting for unfinished jobs....
-      #        > [ 32%] Linking CXX static library libcri_v1alpha2.a
-      #        > [ 32%] Built target cri_v1alpha2
-      #        > [ 33%] Linking CXX static library libcri_v1.a
-      #        > [ 33%] Built target cri_v1
-      #        > make: *** [Makefile:156: all] Error 2
-      #        For full logs, run 'nix log /nix/store/3d6dl3p6vh6q167f476g0jd5k9lf40vx-sysdig-0.33.1.drv'.
-      # error: 1 dependencies of derivation '/nix/store/lya9lrjxyfx1pql568d88x3j9kqsndar-kernel-modules.drv' failed to build
-      # error: 1 dependencies of derivation '/nix/store/08xhqi0rmd4i9i7qm4r559mqmv1k4iff-linux-6.6.2-modules.drv' failed to build
-      # error: 1 dependencies of derivation '/nix/store/hy9c4szjba6mxn9bwa4yxjiv9vbnp657-nixos-system-vno1-oh2-23.11.20231128.7c4c205.drv' failed to build
-      # error: 1 dependencies of derivation '/nix/store/p7rx1li894pfyc6s6nz5f6jdcdjvl3xi-activatable-nixos-system-vno1-oh2-23.11.20231128.7c4c205.drv' failed to build
-      # error: 1 dependencies of derivation '/nix/store/r0szq7sqarjk5mrhhb3w8vn9li8c43lz-deploy-rs-check-activate.drv' failed to build
-      # error: build of '/nix/store/gwc35cfp7ndxyz4vs7i9r123hmbr90r3-jsonschema-deploy-system.drv', '/nix/store/r0szq7sqarjk5mrhhb3w8vn9li8c43lz-deploy-rs-check-activate.drv' failed
-      # 🚀 ❌ [deploy] [ERROR] Failed to check deployment: Nix checking command resulted in a bad exit code: Some(1)
-
-      #sysdig.enable = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
     };
 
     networking.firewall.logRefusedConnections = false;
