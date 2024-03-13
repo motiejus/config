a705428b89df052b932daecaca960d79dab2bf90
Motiejus Jakštys <motiejus@jakstys.lt>
2024-03-13 12:40:31 +0200

    work: add google-cloud-sdk

--- shared/work/default.nix
+++ shared/work/default.nix
@@ -1,5 +1,14 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   mj.base.users.email = "motiejus.jakstys@chronosphere.io";
+
+  environment.systemPackages = with pkgs; [
+    google-cloud-sdk
+  ];
+
   home-manager.users.${config.mj.username} = {
     home.sessionVariables.GOPRIVATE = "github.com/chronosphereio";
     programs.git.extraConfig.url."git@github.com:".insteadOf = "https://github.com";
