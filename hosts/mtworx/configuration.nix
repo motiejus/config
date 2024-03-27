b27202e998390c099b455d115e8ef9239b2a3f61
Motiejus Jakštys <motiejus.jakstys@chronosphere.io>
2024-03-27 09:53:23 +0200

    mtworx: intel_pstate=disable

--- hosts/mtworx/configuration.nix
+++ hosts/mtworx/configuration.nix
@@ -16,7 +16,7 @@ in {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     kernelModules = ["kvm-intel"];
-    kernelParams = ["intel_pstate=disable"];
+    #kernelParams = ["intel_pstate=disable"];
     loader.systemd-boot.enable = true;
     initrd = {
       availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usbhid"];
