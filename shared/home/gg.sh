19e313d567eee2219bf17eabc501690e44dfa62a
Motiejus Jakštys <motiejus@jakstys.lt>
2024-04-12 14:59:47 +0300

    shfmt

--- shared/home/gg.sh
+++ shared/home/gg.sh
@@ -4,24 +4,24 @@ _GG_MAXDEPTH=5
 gg() {
     local _gopath
     _gopath=$(git rev-parse --show-toplevel)
-	local paths=($(g "$@"))
-	local path_index=0
+    local paths=($(g "$@"))
+    local path_index=0
 
-	if [ ${#paths[@]} -gt 1 ]; then
-		local c=1
-		for path in "${paths[@]}"; do
-			echo "[$c]: cd ${_gopath}/${path}"
-			c=$((c+1))
-		done
-		echo -n "Go to which path: "
-		read -r path_index
+    if [ ${#paths[@]} -gt 1 ]; then
+        local c=1
+        for path in "${paths[@]}"; do
+            echo "[$c]: cd ${_gopath}/${path}"
+            c=$((c + 1))
+        done
+        echo -n "Go to which path: "
+        read -r path_index
 
-		path_index=$((path_index-1))
-	fi
+        path_index=$((path_index - 1))
+    fi
 
-	local path=${paths[$path_index]}
-	cd "$_gopath/$path" || {
-        >&2 echo "?"
+    local path=${paths[$path_index]}
+    cd "$_gopath/$path" || {
+        echo >&2 "?"
         exit 1
     }
 }
@@ -32,17 +32,16 @@ gg() {
 g() {
     local pkg_candidates
     pkg_candidates="$( (cd "$_gopath" && find . -mindepth 1 -maxdepth ${_GG_MAXDEPTH} -type d -path "*/$1" -and -not -path '*/vendor/*' -print) | sed 's/^\.\///g')"
-	echo "$pkg_candidates" | awk '{print length, $0 }' | sort -n | awk '{print $2}'
+    echo "$pkg_candidates" | awk '{print length, $0 }' | sort -n | awk '{print $2}'
 }
 #
 # Bash autocomplete for g and gg functions.
 #
-_g_complete()
-{
+_g_complete() {
     COMPREPLY=()
     local cur
     cur="${COMP_WORDS[COMP_CWORD]}"
-    COMPREPLY=( $(compgen -W "$(for f in $(find "$_gopath" -mindepth 1 -maxdepth ${_GG_MAXDEPTH} -type d -name "${cur}*" ! -name '.*' ! -path '*/.git/*' ! -path '*/test/*' ! -path '*/vendor/*'); do echo "${f##*/}"; done)" --  "$cur") )
+    COMPREPLY=($(compgen -W "$(for f in $(find "$_gopath" -mindepth 1 -maxdepth ${_GG_MAXDEPTH} -type d -name "${cur}*" ! -name '.*' ! -path '*/.git/*' ! -path '*/test/*' ! -path '*/vendor/*'); do echo "${f##*/}"; done)" -- "$cur"))
     return 0
 }
 complete -F _g_complete g
