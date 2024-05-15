2d8cbe1450723b490f9624eb284c06473d978a3d
Motiejus Jakštys <motiejus@jakstys.lt>
2024-05-15 20:45:36 +0200

    formatting

--- shared/home/dev.lua
+++ shared/home/dev.lua
@@ -2,5 +2,4 @@ vim.api.nvim_exec([[
     set grepprg=@ripgrep@/bin/rg\ --vimgrep grepformat^=%f:%l:%c:%m
 ]], false)
 
-vim.filetype.add({
-extension = { star = 'python' }})
+vim.filetype.add({extension = {star = 'python'}})
