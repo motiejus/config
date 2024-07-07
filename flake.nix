313eef8300abfc85d7b7b11e9ff7c74531a08fe5
Motiejus Jakštys <motiejus@jakstys.lt>
2024-07-07 08:50:55 +0300

    pre-commit-hooks: remove flake-utils

--- flake.nix
+++ flake.nix
@@ -49,7 +49,6 @@
         nixpkgs.follows = "nixpkgs";
         nixpkgs-stable.follows = "nixpkgs";
         flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
         gitignore.follows = "gitignore";
       };
     };
