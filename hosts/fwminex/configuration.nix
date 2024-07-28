faae4708f90e7c59d5256e83a9b9c1729695fb2a
Motiejus Jakštys <motiejus@jakstys.lt>
2024-07-28 22:46:51 +0300

    deployerbot: fwminex and vno3-rp3b are unconditional

--- hosts/fwminex/configuration.nix
+++ hosts/fwminex/configuration.nix
@@ -106,21 +106,15 @@ in {
           uidgid = myData.uidgid.updaterbot-deployer;
           repo = "git@git.jakstys.lt:motiejus/config";
           deployDerivations = [
+            ".#fwminex"
             ".#vno1-oh2"
             ".#fra1-a"
+            ".#vno3-rp3b"
           ];
           deployIfPresent = [
             {
-              derivationTarget = ".#fwminex";
-              pingTarget = myData.hosts."fwminex.motiejus.jakst".jakstIP;
-            }
-            {
               derivationTarget = ".#mtworx";
               pingTarget = myData.hosts."mtworx.motiejus.jakst".jakstIP;
-            }
-            {
-              derivationTarget = ".#vno3-rp3b";
-              pingTarget = myData.hosts."vno3-rp3b.servers.jakst".jakstIP;
             }
           ];
         };
