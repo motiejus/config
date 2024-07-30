f1204e57dbbdc0f315cc37b0d4860f0a40a30b09
Motiejus Jakštys <motiejus@jakstys.lt>
2024-07-31 00:51:35 +0300

    gc: no more than 7d

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -78,7 +78,7 @@ in
       gc = {
         automatic = true;
         dates = "weekly";
-        options = "--delete-older-than 14d";
+        options = "--delete-older-than 7d";
       };
       settings = {
         experimental-features = [
