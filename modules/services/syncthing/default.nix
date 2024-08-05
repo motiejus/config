0592c8d1fabc2ee6846a17f8683b73d529f7876b
Motiejus Jakštys <motiejus@jakstys.lt>
2024-08-05 21:52:16 +0300

    syncthing: +www-vno1-gdrx

--- modules/services/syncthing/default.nix
+++ modules/services/syncthing/default.nix
@@ -245,7 +245,7 @@ in
           { }
           // (lib.optionalAttrs (config.networking.hostName == "fwminex") {
             "/var/www/dl/tel" = www-mxp10;
-            "/var/www/dl/fwminex" = www-fwminex;
+            "/var/www/dl/www-vno1-gdrx" = www-vno1-gdrx;
             "/var/www/dl/mtworx" = www-mtworx;
             "/var/www/dl/mykolo" = mykolo;
             "${cfg.dataDir}/annex2/Books" = Books;
