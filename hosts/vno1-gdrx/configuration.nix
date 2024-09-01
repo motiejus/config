0944592e27c972035bc0ba19c42ae3a12c41666e
Motiejus Jakštys <motiejus@jakstys.lt>
2024-09-01 05:46:17 +0300

    vno1-gdrx: +librecad

--- hosts/vno1-gdrx/configuration.nix
+++ hosts/vno1-gdrx/configuration.nix
@@ -1,4 +1,9 @@
-{ config, myData, ... }:
+{
+  config,
+  myData,
+  pkgs,
+  ...
+}:
 let
   nvme = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S6P1NX0TA00913P";
 in
@@ -166,6 +171,8 @@ in
 
     };
   };
+
+  environment.systemPackages = with pkgs; [ librecad ];
 
   networking = {
     hostName = "vno1-gdrx";
