6a0b89d330a1fbcfe9a4a8028f2ca785b3c3d9ce
Motiejus Jakštys <motiejus@jakstys.lt>
2024-11-16 01:53:13 +0200

    cups: remove nonworking driver

--- modules/services/printing/default.nix
+++ modules/services/printing/default.nix
@@ -16,7 +16,6 @@ in
     services.printing = {
       enable = true;
       drivers = [
-        pkgs.canon-cups-ufr2
         pkgs.samsung-unified-linux-driver_4_01_17
         (pkgs.writeTextDir "share/cups/model/HP_Color_Laser_15x_Series.ppd" (
           builtins.readFile ../../../shared/HP_Color_Laser_15x_Series.ppd
