acc17ed54c3f33123ec40388e33c8f316bde5a6e
Motiejus Jakštys <motiejus@jakstys.lt>
2025-01-06 20:00:07 +0100

    wip ghostty

--- modules/profiles/desktop/default.nix
+++ modules/profiles/desktop/default.nix
@@ -293,6 +293,18 @@ in
           mbsync.enable = true;
           neomutt.enable = true;
           notmuch.enable = true;
+          #ghostty = {
+          #  enable = true;
+          #  installVimSyntax = true;
+          #  enableBashIntegration = true;
+          #  settings = {
+          #    font-feature = [
+          #      "-calt"
+          #      "-liga"
+          #      "-dlig"
+          #    ];
+          #  };
+          #};
 
           tmux.extraConfig =
             let
