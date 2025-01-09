ffd896d969a0eef883bb136d615d7eea1a7e8148
Motiejus Jakštys <motiejus@jakstys.lt>
2025-01-09 14:36:05 +0200

    flake.nix: remove unused override

--- flake.nix
+++ flake.nix
@@ -54,7 +54,6 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
         flake-compat.follows = "flake-compat";
         gitignore.follows = "gitignore";
       };
