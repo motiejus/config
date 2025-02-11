0047a3eefbad6d22926307d52fda7789d1fd87c4
Motiejus Jakštys <motiejus@jakstys.lt>
2025-02-11 20:50:18 +0200

    frigate: roll back to normal python3

--- hosts/fwminex/configuration.nix
+++ hosts/fwminex/configuration.nix
@@ -7,9 +7,6 @@
 }:
 let
   nvme = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S6P1NS0TA01331A_1";
-  python3-mine = pkgs.python312.overrideAttrs (_: {
-    EXTRA_CFLAGS = " -fno-omit-frame-pointer";
-  });
 in
 {
   imports = [
@@ -118,7 +115,6 @@ in
       frigate = {
         preStart = "ln -sf $CREDENTIALS_DIRECTORY/secrets.env /run/frigate/secrets.env";
         serviceConfig = {
-          ExecStart = lib.mkForce "${lib.getExe python3-mine} -m frigate";
           EnvironmentFile = [ "-/run/frigate/secrets.env" ];
           Environment = [ "PYTHONPERFSUPPORT=1" ];
           RuntimeDirectory = "frigate";
@@ -798,7 +794,6 @@ in
       inferno
       rtorrent
       age-plugin-yubikey
-      python3-mine
     ];
   };
 
