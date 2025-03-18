8fb9bd93dd5f30b0f4aac3d67120c17c3777842c
Motiejus Jakštys <motiejus@jakstys.lt>
2025-03-18 09:47:29 +0000

    frigate: -motorcycle

--- modules/services/frigate/default.nix
+++ modules/services/frigate/default.nix
@@ -153,7 +153,6 @@ in
                   "bicycle"
                   "car"
                   "dog"
-                  "motorcycle"
                   "person"
                 ];
               };
