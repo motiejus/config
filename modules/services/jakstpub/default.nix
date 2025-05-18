69be595c56bed9267cde1b457133cc76bba972a0
Motiejus Jakštys <motiejus@jakstys.lt>
2025-05-18 19:45:00 +0000

    fix typo

--- modules/services/jakstpub/default.nix
+++ modules/services/jakstpub/default.nix
@@ -82,7 +82,7 @@ in
         dataDir = "/data/vno3-shared";
         devices = {
           vno1-gdrx.id = myData.hosts."vno1-gdrx.jakst.vpn".syncthingID;
-          mtworx.id = myData.hosts."mtsorx.jakst.vpn".syncthingID;
+          mtworx.id = myData.hosts."mtworx.jakst.vpn".syncthingID;
         };
         folders = {
           "/data/vno3-shared/Zemelapiai" = {
