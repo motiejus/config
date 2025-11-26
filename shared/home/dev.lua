8aaa536a2af18bb9a0ddfddcb01375b724a7a53c
Motiejus Jakštys <motiejus@jakstys.lt>
2025-11-26 07:38:51 +0000

    lspconfig: add `nicer` wrappers

--- shared/home/dev.lua
+++ shared/home/dev.lua
@@ -1,4 +1,10 @@
-require'lspconfig'.clangd.setup{}
+require'lspconfig'.clangd.setup{
+    cmd = { "nicer", "clangd" }
+}
+
+require'lspconfig'.gopls.setup{
+    cmd = { "nicer", "gopls" }
+}
 
 vim.api.nvim_exec([[
     set grepprg=@ripgrep@/bin/rg\ --vimgrep grepformat^=%f:%l:%c:%m
