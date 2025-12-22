d8a8fa69a66661c397c8c6e15ab0177d26a35da9
Motiejus Jakštys <motiejus@jakstys.lt>
2025-12-22 08:56:38 +0000

    re-enable clangd et al

--- shared/home/dev.lua
+++ shared/home/dev.lua
@@ -1,11 +1,18 @@
 -- Updated to use vim.lsp.config (nvim 0.11+) instead of deprecated lspconfig
 vim.lsp.config.clangd = {
-    cmd = { "nicer", "clangd" }
+    cmd = { "nicer", "clangd" },
+    filetypes = { "c", "cpp" },
+    root_markers = { "compile_commands.json", ".clangd", ".git" }
 }
 
 vim.lsp.config.gopls = {
-    cmd = { "nicer", "@gopls@/bin/gopls" }
+    cmd = { "nicer", "@gopls@/bin/gopls" },
+    filetypes = { "go" },
+    root_markers = { "go.mod", ".git" }
 }
+
+-- Enable the LSP servers
+vim.lsp.enable({ "clangd", "gopls" })
 
 vim.api.nvim_exec([[
     set grepprg=@ripgrep@/bin/rg\ --vimgrep grepformat^=%f:%l:%c:%m
