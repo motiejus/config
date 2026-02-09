9be9e59597f26e6246ed60a8b6b71c35c577cbda
Motiejus Jakštys <motiejus@jakstys.lt>
2026-02-09 14:53:16 +0000

    revert to copying

--- hosts/mtworx/configuration.nix
+++ hosts/mtworx/configuration.nix
@@ -34,7 +34,7 @@ let
     boot
 
     :debian-shell-nfs
-    kernel http://10.14.143.1/boot/debian-xfce/live/vmlinuz boot=live components netboot=nfs nfsroot=10.14.143.1:/srv/boot/debian-xfce systemd.unit=multi-user.target ''${cmdline}
+    kernel http://10.14.143.1/boot/debian-xfce/live/vmlinuz boot=live components netboot=nfs nfsroot=10.14.143.1:/boot/debian-xfce systemd.unit=multi-user.target ''${cmdline}
     initrd http://10.14.143.1/boot/debian-xfce/live/initrd.img
     boot
 
@@ -44,7 +44,7 @@ let
     boot
 
     :debian-xfce-nfs
-    kernel http://10.14.143.1/boot/debian-xfce/live/vmlinuz boot=live components netboot=nfs nfsroot=10.14.143.1:/srv/boot/debian-xfce ''${cmdline}
+    kernel http://10.14.143.1/boot/debian-xfce/live/vmlinuz boot=live components netboot=nfs nfsroot=10.14.143.1:/boot/debian-xfce ''${cmdline}
     initrd http://10.14.143.1/boot/debian-xfce/live/initrd.img
     boot
 
@@ -93,18 +93,28 @@ let
   };
 
   exportsFile = pkgs.writeText "unfs3-exports" ''
-    /srv/boot 10.14.143.0/24(ro,no_subtree_check,no_root_squash,insecure) localhost(ro,no_subtree_check,no_root_squash,insecure)
+    /boot 10.14.143.0/24(ro,no_subtree_check,no_root_squash,insecure) localhost(ro,no_subtree_check,no_root_squash,insecure)
   '';
 
   # TFTP root directory with all boot files
   tftp-root = pkgs.runCommand "tftp-root" { } ''
-    mkdir -p $out
+    mkdir -p $out/alpine
+    mkdir -p $out/debian-xfce
+    mkdir -p $out/nixos
 
-    ln -s ${customIpxeEfi}/ipxe.efi $out/boot.efi
-    ln -s ${customIpxeBios}/undionly.kpxe $out/boot.kpxe
-    ln -s ${pkgs.mrescue-alpine} $out/alpine
-    ln -s ${pkgs.mrescue-debian-xfce} $out/debian-xfce
-    ln -s ${pkgs.mrescue-nixos} $out/nixos
+    cp ${customIpxeEfi}/ipxe.efi $out/boot.efi
+    cp ${customIpxeBios}/undionly.kpxe $out/boot.kpxe
+
+    # Alpine
+    cp ${pkgs.mrescue-alpine}/kernel $out/alpine/kernel
+    cp ${pkgs.mrescue-alpine}/initrd $out/alpine/initrd
+
+    # Debian XFCE (full ISO contents)
+    cp -r ${pkgs.mrescue-debian-xfce}/* $out/debian-xfce/
+
+    # NixOS
+    cp ${pkgs.mrescue-nixos}/kernel $out/nixos/kernel
+    cp ${pkgs.mrescue-nixos}/initrd $out/nixos/initrd
   '';
 in
 {
@@ -284,12 +294,7 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${pkgs.unfs3}/bin/unfsd -e ${exportsFile} -s -d -n 2049 -m 20048";
-        TemporaryFileSystem = [ "/srv/boot:ro" ];
-        BindReadOnlyPaths = [
-          "${pkgs.mrescue-alpine}:/srv/boot/alpine"
-          "${pkgs.mrescue-debian-xfce}:/srv/boot/debian-xfce"
-          "${pkgs.mrescue-nixos}:/srv/boot/nixos"
-        ];
+        BindReadOnlyPaths = [ "${tftp-root}:/boot" ];
         DynamicUser = true;
         ProtectHome = true;
         ProtectSystem = "strict";
