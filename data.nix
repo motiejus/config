e7e1aa460d304d3f9a4542f9ac7cae44123e2c64
Motiejus Jakštys <motiejus@jakstys.lt>
2026-03-25 07:15:11 +0000

    add acme-challenge to top-level jakstys.lt

--- data.nix
+++ data.nix
@@ -207,6 +207,10 @@ rec {
       _dmarc                          TXT    "v=DMARC1; p=none;"
       google._domainkey               TXT    "v=DKIM1; k=rsa;" "p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuqOyONnWKk7lgAVB1UcVu/I02gTDjROpQGDNUJHS34faQ9DnM/8uSOaIwCe4oV1GrI8N2ET+f96WPCCs1LzlEA0QwuUoXRLGojjQoXxCntLfMCnRWtehzmZq6Yv8nVva7N0gz/n/LThpPvGfEoKzYjmhjzM5d8y60DGsKxS8r4Lc9TzwtzuYkxKDhcSzVBQQiMvKMi6m6mUsxFya7" "ZTurd5i7iiZXpA3SFBYLAsjhQd6vS7K13vwAZTKjGNijfM40i7KXC5XA5WtojiSY0lZzAMqaHGLDaMUFkWRJJntRheQ+AU9RvOGAufphRAjdQTCMy0BLzC0rilT2JaTGe4MdQIDAQAB"
 
+      _acme-challenge                  CNAME     _acme-endpoint
+      _acme-endpoint                      NS     ns._acme-endpoint
+      ns._acme-endpoint                    A     ${vno1}
+
       grafana                             A     ${hosts."fwminex.jakst.vpn".jakstIP}
       _acme-challenge.grafana         CNAME     _acme-endpoint.grafana
       _acme-endpoint.grafana             NS     ns._acme-endpoint.grafana
