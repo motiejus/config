53a4772e497d6aab3213b562de290fbf9dc2517f
Motiejus Jakštys <motiejus@jakstys.lt>
2026-09-14 08:30:28 +0300

    lt-maps: remove .#check

--- modules/services/lt-shelters/default.nix
+++ modules/services/lt-shelters/default.nix
@@ -159,7 +159,6 @@ let
       git -C "$lt_maps_repo" config user.email lt-maps@jakstys.lt
       git -C "$lt_maps_repo" add -- nix/data-sources.json
       if ! git -C "$lt_maps_repo" diff --cached --quiet; then
-        (cd "$lt_maps_repo"; nix build --no-warn-dirty --accept-flake-config --no-link .#check)
         git -C "$lt_maps_repo" commit -m "Update Lithuania map data sources"
         GIT_DEFAULT_HASH=sha1 git -C "$lt_maps_repo" push origin main
       fi
